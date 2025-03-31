36c2011abad940c1e97e90e654a10d239992a517
ARnishad45 <arnishad4518@gmail.com>
2025-03-31 21:26:42 +0530

    Added Signup Feature

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -7,7 +7,11 @@ import { BASE_URL } from "../utils/constants";
 
 const Login = () => {
   const [email, setEmail] = useState("mark69@gmail.com");
-  const [password, setPassword] = useState("mark@123");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [password, setPassword] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [isLogin, setIsLogin] = useState(true);
   const [error, setError] = useState();
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -24,96 +28,108 @@ const Login = () => {
       );
       // console.log(res.data.data);
       dispatch(addUser(res.data.data));
-      return navigate("/feed");
+      return navigate("/");
     } catch (err) {
-      setError(err?.res?.data || "Something went wrong!");// fix the validations 
+      setError(err?.response?.data?.message || "An unexpected error occurred");// fix the validations 
       // console.error(err);
+    }
+  };
+
+  const handelSignUp = async () => {
+    try {
+
+      const res = await axios.post(BASE_URL + "/signup", {
+        firstName,
+        lastName,
+        email,  
+        password,
+        phoneNumber
+      }, { withCredentials: true });
+
+      console.log(res.data.data);
+      dispatch(addUser(res.data.data));
+      return navigate("/profile");
+
+    } catch (err) {
+      setError(err?.response?.data?.message || "An unexpected error occurred");
     }
   };
 
   return (
     <>
-      <div className="flex justify-center my-32">
-        <div className="card bg-base-200 text-neutral-content w-96">
-          <div className="card-body items-center text-center">
-            <h1 className="card-title">Login</h1>
-            <label className="input validator">
-              <svg
-                className="h-[1em] opacity-50"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-              >
-                <g
-                  strokeLinejoin="round"
-                  strokeLinecap="round"
-                  strokeWidth="2.5"
-                  fill="none"
-                  stroke="currentColor"
-                >
-                  <rect width="20" height="16" x="2" y="4" rx="2"></rect>
-                  <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
-                </g>
-              </svg>
-              <input
-                type="email"
-                placeholder="xyz@mail.com"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                required
-              />
-            </label>
-            <div className="validator-hint hidden">
-              Enter valid email address
+
+      <div className="relative flex flex-col items-center justify-center h-screen overflow-hidden">
+        <div className="w-full p-6 bg-base-300 border-t-4 border-gray-300 rounded-md shadow-md border-top lg:max-w-md">
+          <h2 className="text-2xl font-semibold text-center text-gray-300">
+            {isLogin ? "Login" : "Sign Up"}
+          </h2>
+          <form className="space-y-4">
+            {
+              <>
+                <div>
+                  <label className="label">
+                    <span className="text-base label-text">Email</span>
+                  </label>
+                  <input type="text"
+                    onChange={(e) => setEmail(e.target.value)}
+                    value={email}
+                    placeholder="Email Address" className="w-full input input-bordered" />
+                </div>
+                <div>
+                  <label className="label">
+                    <span className="text-base label-text">Password</span>
+                  </label>
+                  <input type="password"
+                    onChange={(e) => setPassword(e.target.value)}
+                    value={password}
+                    placeholder="Enter Password" className="w-full input input-bordered" />
+                </div>
+              </>
+            }
+
+            {
+              !isLogin &&
+              <>
+                <div>
+                  <label className="label">
+                    <span className="text-base label-text">First Name</span>
+                  </label>
+                  <input type="text"
+                    onChange={(e) => setFirstName(e.target.value)}
+                    value={firstName}
+                    placeholder="First Name" className="w-full input input-bordered" />
+                </div>
+                <div>
+                  <label className="label">
+                    <span className="text-base label-text">Last Name</span>
+                  </label>
+                  <input type="text"
+                    onChange={(e) => setLastName(e.target.value)}
+                    value={lastName}
+                    placeholder="Enter Password" className="w-full input input-bordered" />
+                </div>
+                <div>
+                  <label className="label">
+                    <span className="text-base label-text">Phone Number</span>
+                  </label>
+                  <input type="text"
+                    onChange={(e) => setPhoneNumber(e.target.value)}
+                    value={phoneNumber}
+                    placeholder="Phone Number" className="w-full input input-bordered" />
+                </div>
+              </>
+            }
+
+            <div>
+              <button className="btn btn-block btn-primary" onClick={(e) => {
+                e.preventDefault(); 
+                isLogin ? handelLogin() : handelSignUp();
+              }}
+              >{isLogin ? "Login" : "Sign Up"}</button>
             </div>
 
-            <label className="input validator">
-              <svg
-                className="h-[1em] opacity-50"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-              >
-                <g
-                  strokeLinejoin="round"
-                  strokeLinecap="round"
-                  strokeWidth="2.5"
-                  fill="none"
-                  stroke="currentColor"
-                >
-                  <path d="M2.586 17.414A2 2 0 0 0 2 18.828V21a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h1a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h.172a2 2 0 0 0 1.414-.586l.814-.814a6.5 6.5 0 1 0-4-4z"></path>
-                  <circle
-                    cx="16.5"
-                    cy="7.5"
-                    r=".5"
-                    fill="currentColor"
-                  ></circle>
-                </g>
-              </svg>
-              <input
-                type="password"
-                required
-                placeholder="Password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                // minlength="6"
-                // pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}"
-                // title="Must be more than 8 characters, including number, lowercase letter, uppercase letter"
-              />
-            </label>
-            <p className="validator-hint hidden">
-              Must be more than 6 characters, including
-              <br />
-              At least one number
-              <br />
-              At least one lowercase letter
-              <br />
-              At least one uppercase letter
-            </p>
-
-            <p className="text-red-500 ">{error}</p>
-            <button className="btn btn-primary" onClick={() => handelLogin()}>
-              Log In
-            </button>
-          </div>
+            <p className="text-s text-center text-gray-400 hover:text-blue-500 " onClick={() => setIsLogin((value) => !value)}>{isLogin ? "Don't have an account? Sign Up" : "Already have an account? Login"}</p>
+          </form>
         </div>
       </div>
     </>
